61702fbd76f388d6efd1c9de4fde28ad4ad1b6b9
wheatdog <wheatdoge@gamil.com>
2017-06-07 22:09:17 +0800

    Fix some db issue

--- db.py
+++ db.py
@@ -2,8 +2,8 @@ from flask.ext.mysql import MySQL
 
 mysql = MySQL()
 def init_mysql(app):
-    app.config['MYSQL_DATABASE_USER'] = 'root'
-    app.config['MYSQL_DATABASE_PASSWORD'] = ''
+    app.config['MYSQL_DATABASE_USER'] = 'cnl'
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'cnl'
     app.config['MYSQL_DATABASE_DB'] = 'cnl'
     app.config['MYSQL_DATABASE_HOST'] = 'localhost'
     mysql.init_app(app)
